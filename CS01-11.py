d6a97d6296baa2e04eedc69a4a5d69c5b701e00f
Roryon Wannapraserd <105912875+oporekungthai@users.noreply.github.com>
2022-09-18 23:00:56 +0700

    Create CS01-11.py

---
+++ CS01-11.py
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+    for(int i=1;i<6;i++){
+        for(int a =0;a<i;a++){
+            cout << ".";
+        }
+        cout << endl;
+
+    }
+}
