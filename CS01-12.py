86e2d7abe4724ce4fd02b4bd976160b6c784d4a8
Roryon Wannapraserd <105912875+oporekungthai@users.noreply.github.com>
2022-09-18 22:51:18 +0700

    Create CS01-12.py

---
+++ CS01-12.py
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  string info[3];
+  cin >> info[0] >> info[1] >> info[2];
+  for(int i =0;i<3;i++){
+    cout << info[i] << endl;
+
+
+
+  }
+  
+
+}
